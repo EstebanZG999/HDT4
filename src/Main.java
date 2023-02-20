a3f5fa687ce89f4230e977218b8d67f6f4723b73
EstebanZG999 <zam22119@uvg.edu.gt>
2023-02-19 18:16:54 -0600

    Primer commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+/**
+ * Universidad del Valle de guatemala
+ * Estructuras de Datos
+ *
+ * Hoja de trabajo 4
+ *
+ * @author Esteban Zambrano 22119
+ * @since 17/02/2023
+ */
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Controlador control = new Controlador();
+
+        //C:\Users\Usuario\Desktop\test.txt
+
     }
 }
